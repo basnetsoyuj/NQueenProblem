412771ec4c65c26330074fe9ed965b2035bbacf9
SoyujBasnet <bsoyuj@gmail.com>
2018-08-13 22:53:19 +0545

    Added Comments making it more readable and README.md

---
+++ README.md
@@ -0,0 +1,12 @@
+# N Queens Problem Solver
+This simple python script finds different possible ways in which N queens can be arranged on the board of (NXN) without any threats to one another.
+i.e No checks to one another
+
+## Example
+8 queens
+1 possibility :queens on a4,b8,c1,d3,e6,f2,g7,h5
+![queen](https://user-images.githubusercontent.com/18349557/42434219-0df75fb6-8372-11e8-887d-6c96bbb7e51c.png)
+
+## Running as script
+The ```solver.py``` file can be run independently as a script as :-
+```<pythonInterpreter> solver.py```
--- solver.py
+++ solver.py
@@ -1,31 +1,37 @@
+# the n*n board
 n=10
+# Returns the check coordinates for a position (x,y) of the Queen
 def check_pos(x,y):
     def diagonal_handler(l):
         return list(filter(lambda tup: not ((tup[0] <= 0) or (tup[0] > n)) and not ((tup[1] <= 0) or (tup[1] > n)),l))
     return set([(x,j) for j in range(1,n+1)]+[(i,y) for i in range(1,n+1)]+diagonal_handler([(x+i,y+i) for i in range(-2*n,2*n)])\
            +diagonal_handler([(x+i,y-i) for i in range(-2*n,2*n)])\
            +diagonal_handler([(x-i,y+i) for i in range(-2*n,2*n)])+diagonal_handler([(x-i,y-i) for i in range(-2*n,2*n)]))
+#preloading all check positions for optimization
 total_set={(x,y):check_pos(x,y)for x in range(1,n+1)for y in range(1,n+1)}
+#returns check position for a list of queen position
 def connector(list_):
     check_places=set()
     [check_places.update(list(total_set[x])) for x in list_]
     return check_places
+
 def solver(q_list,iter_):
-    if iter_ == n :
+    #for the last iteration ie till [(1,.).......(n-1,..)] is decided
+    if iter_ == n :#checks if the n-1 positions are correct
         check_places=connector(q_list)
         q_rem = (n,{x for x in range(1, n + 1)}.difference({x[1] for x in q_list}).pop())
         if not(q_rem in check_places):
             q_list.append(q_rem)
-            print(q_list)
+            print(q_list)#if so prints the last remaining valid move
     else:
-        for i in range(1,n+1):
+        for i in range(1,n+1):#recursion of a new loop inside loops for solcing
             check_position=connector(q_list)
-            if (iter_,i) in check_position :
-                q_list_lier=[x for x in q_list if x[0]==iter_]
-                if q_list_lier:
-                    q_list.remove(q_list_lier.pop())
+            if (iter_,i) in check_position :#check if the new position clashes the old queens check position
+                q_list_lier=[x for x in q_list if x[0]==iter_]#later when the first solution on any row is found,it skips
+                if q_list_lier:                               #because the q_list contains last old queen position
+                    q_list.remove(q_list_lier.pop())          #where it was solved , so remove it if it exists
                 continue
             q_list.append((iter_,i))
-            solver(q_list,iter_+1)
-            q_list.pop()
-[solver(q_list=[(1,y)],iter_=2) for y in range(1,n+1) ]
+            solver(q_list,iter_+1)#recusion line
+            q_list.pop()# remove the recent test queen data at the end of loop of that queen row
+[solver(q_list=[(1,y)],iter_=2) for y in range(1,n+1) ]#run the first iteration from outside for simplicity
